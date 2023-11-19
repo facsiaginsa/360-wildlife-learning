c46e3001dd79165f6fd6dd2554ecb88e051e4b20
Bagus F <facsi.aginsa@gmail.com>
2023-11-19 20:07:20 +0700

    add default asset url

--- src/configs/index.js
+++ src/configs/index.js
@@ -3,5 +3,5 @@ require("dotenv").config()
 module.exports = {
     PORT: process.env.PORT || 3000,
     HOST: process.env.HOST || "0.0.0.0",
-    ASSET_URL: process.env.ASSET_URL
+    ASSET_URL: process.env.ASSET_URL || "https://wildlife-asset.facsiaginsa.com"
 }
